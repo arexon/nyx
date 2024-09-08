4e68d802389afbf94d4c37da8633eafe642e76ce
arexon <arexonreal@gmail.com>
2024-09-08 11:22:30 +0300

    chore: formatting

--- overlays/spotify/default.nix
+++ overlays/spotify/default.nix
@@ -1,30 +1,29 @@
 _: prev:
-with prev; {
-  spotify = let
-    version = "1.0.3";
-    spotify-adblock = fetchurl {
-      name = "spotify-adblock-${version}";
+with prev; let
+  version = "1.0.3";
+  spotify-adblock = fetchurl {
+    name = "spotify-adblock";
 
-      url = "https://github.com/abba23/spotify-adblock/releases/download/v${version}/spotify-adblock.so";
+    url = "https://github.com/abba23/spotify-adblock/releases/download/v${version}/spotify-adblock.so";
 
-      sha256 = "sha256-pOQVhDOPzqE/R15PYrMb8tKs68/W/vpbt1Jz+N15LJs=";
+    sha256 = "sha256-pOQVhDOPzqE/R15PYrMb8tKs68/W/vpbt1Jz+N15LJs=";
 
-      downloadToTemp = true;
+    downloadToTemp = true;
 
-      recursiveHash = true;
+    recursiveHash = true;
 
-      postFetch = ''
-        mkdir -p $out/lib
-        mv $downloadedFile $out/lib/spotify-adblock.so
+    postFetch = ''
+      mkdir -p $out/lib
+      mv $downloadedFile $out/lib/spotify-adblock.so
+    '';
+  };
+in {
+  spotify = spotify.overrideAttrs (prev: {
+    installPhase =
+      prev.installPhase
+      + ''
+        wrapProgram $out/share/spotify/spotify \
+          --set LD_PRELOAD "${spotify-adblock}/lib/spotify-adblock.so"
       '';
-    };
-  in
-    spotify.overrideAttrs (prev: {
-      installPhase =
-        prev.installPhase
-        + ''
-          wrapProgram $out/share/spotify/spotify \
-            --set LD_PRELOAD "${spotify-adblock}/lib/spotify-adblock.so"
-        '';
-    });
+  });
 }

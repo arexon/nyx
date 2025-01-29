add22f9f86fc79fa3007c87f93a01fe6c1c7a462
arexon <arexonreal@gmail.com>
2025-01-29 13:58:43 +0200

    fix(mcpelauncher): patch hard-coded paths for zenity and xdg-open

--- modules/home/games/mcpelauncher/default.nix
+++ modules/home/games/mcpelauncher/default.nix
@@ -7,37 +7,41 @@
 with lib; let
   cfg = config.nyx.games.mcpelauncher;
 
-  # We must wrap it inside an FHS environment because of zenity. See:
-  # <https://github.com/minecraft-linux/file-picker/blob/f5cbac6249e3fafd39959b7ab9714dda7048aa42/src/file_picker_zenity.cpp#L10>
-  mcpelauncher-ui-qt-wrapped = pkgs.buildFHSEnv {
-    name = "mcpelauncher-ui-qt";
-    targetPkgs = pkgs: [pkgs.mcpelauncher-ui-qt pkgs.zenity];
-    extraMounts = [
-      {
-        source = "${pkgs.zenity}/bin/zenity";
-        target = "/usr/bin/zenity";
-      }
-    ];
-  };
+  mcpelauncher-client = pkgs.mcpelauncher-client.overrideAttrs (prev:
+    with pkgs; {
+      # Remove once <https://github.com/NixOS/nixpkgs/pull/377734> is on `nixos-unstable`.
+      postPatch = lib.optionalString stdenv.isLinux ''
+        substituteInPlace mcpelauncher-client/src/jni/main_activity.cpp \
+          --replace-fail /usr/bin/xdg-open ${xdg-utils}/bin/xdg-open \
+          --replace-fail /usr/bin/zenity ${zenity}/bin/zenity
+
+        substituteInPlace file-picker/src/file_picker_zenity.cpp \
+          --replace-fail /usr/bin/zenity ${zenity}/bin/zenity
+      '';
+    });
 in {
   options.nyx.games.mcpelauncher = {
     enable = mkBoolOpt false "Whether to install Minecraft Bedrock Launcher.";
   };
 
   config = mkIf cfg.enable {
-    xdg.desktopEntries = {
-      minecraft = {
-        name = "Minecraft";
-        exec = ''${mcpelauncher-ui-qt-wrapped}/bin/mcpelauncher-ui-qt -c "mcpelauncher-ui-qt -p"'';
-        icon = "minecraft";
-        categories = ["Game"];
-      };
-      mcpelauncher-ui-qt = {
-        name = "Minecraft Bedrock Launcher";
-        exec = ''${mcpelauncher-ui-qt-wrapped}/bin/mcpelauncher-ui-qt -c "mcpelauncher-ui-qt"'';
-        icon = "mcpelauncher-ui-qt";
-        categories = ["Game"];
-      };
+    home.packages = with pkgs; [
+      (mcpelauncher-ui-qt.overrideAttrs (_: {
+        preFixup = ''
+          qtWrapperArgs+=(
+            --prefix PATH : ${lib.makeBinPath [mcpelauncher-client]}
+            --unset QT_STYLE_OVERRIDE
+          )
+        '';
+      }))
+      mcpelauncher-client
+    ];
+
+    xdg.desktopEntries.minecraft = {
+      name = "Minecraft";
+      exec = "mcpelauncher-ui-qt -p";
+      icon = "minecraft";
+      categories = ["Game"];
     };
   };
 }

8fc27618881612575405dbdc818b6b099c078726
arexon <arexonreal@gmail.com>
2025-02-01 01:03:57 +0200

    refactor(mcpelauncher): remove local paths patch

--- modules/home/games/mcpelauncher/default.nix
+++ modules/home/games/mcpelauncher/default.nix
@@ -6,19 +6,6 @@
 }:
 with lib; let
   cfg = config.nyx.games.mcpelauncher;
-
-  mcpelauncher-client = pkgs.mcpelauncher-client.overrideAttrs (prev:
-    with pkgs; {
-      # Remove once <https://github.com/NixOS/nixpkgs/pull/377734> is on `nixos-unstable`.
-      postPatch = lib.optionalString stdenv.isLinux ''
-        substituteInPlace mcpelauncher-client/src/jni/main_activity.cpp \
-          --replace-fail /usr/bin/xdg-open ${xdg-utils}/bin/xdg-open \
-          --replace-fail /usr/bin/zenity ${zenity}/bin/zenity
-
-        substituteInPlace file-picker/src/file_picker_zenity.cpp \
-          --replace-fail /usr/bin/zenity ${zenity}/bin/zenity
-      '';
-    });
 in {
   options.nyx.games.mcpelauncher = {
     enable = mkBoolOpt false "Whether to install Minecraft Bedrock Launcher.";
@@ -26,14 +13,7 @@ in {
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      (mcpelauncher-ui-qt.overrideAttrs (_: {
-        preFixup = ''
-          qtWrapperArgs+=(
-            --prefix PATH : ${lib.makeBinPath [mcpelauncher-client]}
-            --unset QT_STYLE_OVERRIDE
-          )
-        '';
-      }))
+      mcpelauncher-ui-qt
       mcpelauncher-client
     ];
 

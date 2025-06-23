c1d8a02f442ceb0b7b0794fb36f7611ac050c92b
arexon <arexonreal@gmail.com>
2025-06-23 10:11:11 +0300

    refactor(spotify): switch to normal spotify

--- modules/home/apps/spotify/default.nix
+++ modules/home/apps/spotify/default.nix
@@ -8,24 +8,18 @@
 with lib; let
   cfg = config.nyx.apps.spotify;
 in {
-  imports = [
-    inputs.spicetify-nix.homeManagerModules.default
-  ];
-
   options.nyx.apps.spotify = {
     enable = mkBoolOpt false "Whether to enable Spotify.";
   };
 
   config = mkIf cfg.enable {
-    programs.spicetify = let
-      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-    in {
-      enable = true;
-      enabledExtensions = with spicePkgs.extensions; [
-        adblock
-        keyboardShortcut
-        betterGenres
-      ];
-    };
+    home.packages = [
+      (pkgs.spotify.overrideAttrs (oldAttrs: {
+        postFixup = ''
+          substituteInPlace $out/share/applications/spotify.desktop \
+            --replace-fail "Exec=spotify %U" "Exec=env NIXOS_OZONE_WL= spotify %U"
+        '';
+      }))
+    ];
   };
 }

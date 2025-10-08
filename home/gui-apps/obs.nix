a7060dbab7b8c8736b0b4c8ba9239c1ab31a703c
arexon <arexonreal@gmail.com>
2025-10-08 12:24:42 +0300

    feat(obs): theme via stylix

--- home/gui-apps/obs.nix
+++ home/gui-apps/obs.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
@@ -6,4 +10,42 @@
       input-overlay
     ];
   };
+
+  xdg.configFile."obs-studio/themes/kanagawa.ovt".text = with config.lib.stylix.colors.withHashtag; ''
+    @OBSThemeMeta {
+        name: 'Kanagawa';
+        id: 'com.obsproject.Yami.Kanagawa';
+        extends: 'com.obsproject.Yami';
+        author: 'arexon';
+        dark: 'true';
+    }
+
+    @OBSThemeVars {
+        --grey1: ${base04};
+        --grey2: ${base04};
+        --grey3: ${base03};
+        --grey4: ${base03};
+        --grey5: ${base02};
+        --grey6: ${base02};
+        --grey7: ${base00};
+        --grey8: ${base01};
+
+        --white1: ${base05};
+
+        --primary_lighter: ${base0E};
+        --primary_light: ${base0E};
+        --primary: ${base0E};
+        --primary_dark: ${base0E};
+        --primary_darker: ${base0E};
+
+        --palette_link: ${base0D};
+        --palette_linkVisited: ${base0D};
+
+        --text: ${base06};
+        --text_light: ${base05};
+        --text_muted: ${base07};
+        --text_disabled: var(--text_muted);
+        --text_inactive: var(--text_muted);
+    }
+  '';
 }

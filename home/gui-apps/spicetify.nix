0847141651c54764546ccf1228a883f266e6fc6e
arexon <arexonreal@gmail.com>
2025-10-12 14:31:16 +0300

    feat(spicetify): install shuffle extension

--- home/gui-apps/spicetify.nix
+++ home/gui-apps/spicetify.nix
@@ -1,6 +1,13 @@
 {
+  inputs,
+  pkgs,
+  ...
+}: let
+  spicePkgs = inputs.spicetify.legacyPackages.${pkgs.system};
+in {
   programs.spicetify = {
     enable = true;
     wayland = true;
+    enabledExtensions = with spicePkgs.extensions; [shuffle];
   };
 }

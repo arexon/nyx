edc949b6bbfae98c69e97c5d7cf8efa802937f2a
arexon <arexonreal@gmail.com>
2025-10-29 14:39:17 +0300

    feat(games): add desktop entry for mcbe

--- home/games.nix
+++ home/games.nix
@@ -4,4 +4,11 @@
     prismlauncher
     mcpelauncher-ui-qt
   ];
+
+  xdg.desktopEntries.minecraft = {
+    name = "Minecraft Bedrock";
+    icon = "minecraft";
+    exec = "mcpelauncher-ui-qt -p";
+    categories = ["Application" "Game"];
+  };
 }

a942b2f43316fba0c4df7663e3001943927b94eb
arexon <arexonreal@gmail.com>
2025-11-06 23:42:02 +0200

    feat(games): add a deskop entry for wine-gdk

--- home/games.nix
+++ home/games.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib) getExe;
+in {
   home.packages = with pkgs; [
     mangohud
     prismlauncher
@@ -10,6 +16,27 @@
     name = "Minecraft Bedrock";
     icon = "minecraft";
     exec = "mcpelauncher-ui-qt -p";
+    categories = ["Application" "Game"];
+  };
+
+  xdg.desktopEntries.minecraft-gdk = let
+    run-mc-gdk = pkgs.writeTextFile rec {
+      name = "run-mc-gdk";
+      destination = "/bin/${name}";
+      executable = true;
+      text = ''
+        #!/usr/bin/env nu
+
+        let id = job spawn { wine ~/games/minecraft-gdk/Minecraft.Windows.exe }
+        sleep 1sec
+        while not (niri msg --json windows | from json | where app_id == "minecraft.windows.exe" | is-empty) { sleep 2sec }
+        job kill $id
+      '';
+    };
+  in {
+    name = "Minecraft Bedrock for Windows";
+    icon = "minecraft";
+    exec = getExe run-mc-gdk;
     categories = ["Application" "Game"];
   };
 }

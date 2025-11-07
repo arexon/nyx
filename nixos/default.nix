8ca64de87f9d0158004f369de2f6eb4b34c499db
arexon <arexonreal@gmail.com>
2025-11-08 00:12:07 +0200

    refactor(nixos): change LC_TIME

--- nixos/default.nix
+++ nixos/default.nix
@@ -101,7 +101,7 @@ in {
   ];
 
   time.timeZone = "Africa/Cairo";
-  i18n.extraLocaleSettings.LC_TIME = "en_IE.UTF-8";
+  i18n.extraLocaleSettings.LC_TIME = "en_US.UTF-8";
 
   fonts.packages = with pkgs; [
     noto-fonts

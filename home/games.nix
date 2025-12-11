a90077fb7e00814f2d12aabe872262f31f68b76b
arexon <arexonreal@gmail.com>
2025-12-11 15:42:55 +0200

    refactor(games): remove mcpelauncher

--- home/games.nix
+++ home/games.nix
@@ -2,13 +2,5 @@
   home.packages = with pkgs; [
     mangohud
     prismlauncher
-    mcpelauncher-ui-qt
   ];
-
-  xdg.desktopEntries.minecraft = {
-    name = "Minecraft Bedrock";
-    icon = "minecraft";
-    exec = "mcpelauncher-ui-qt -p";
-    categories = ["Application" "Game"];
-  };
 }

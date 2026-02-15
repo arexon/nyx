969ca3a4a7181d40bfd218db85b0cc6719139bea
arexon <me@arexon.dev>
2026-02-15 16:50:02 +0200

    chore(packages): update proton-gdk-bin

--- packages/proton-gdk-bin.nix
+++ packages/proton-gdk-bin.nix
@@ -4,12 +4,12 @@
 }:
 proton-ge-bin.overrideAttrs {
   src = fetchzip {
-    url = "https://github.com/Weather-OS/GDK-Proton/releases/download/release/GE-Proton10-25.tar.gz";
-    hash = "sha256-2ShpJjvf0tw+AnjMOwyHllfdxKO6kRGJpackeWOo7iM=";
+    url = "https://github.com/Weather-OS/GDK-Proton/releases/download/release10-29/GDK-Proton10-29.tar.gz";
+    hash = "sha256-MtasTr8lXBY0vQlOPaXU94CO6O//l1qwAqFDbu5os+M=";
   };
 
   preFixup = ''
     substituteInPlace "$steamcompattool/compatibilitytool.vdf" \
-      --replace-fail "GE-Proton10-25" "GDK-Proton"
+      --replace-fail "GE-Proton10-29" "GDK-Proton"
   '';
 }

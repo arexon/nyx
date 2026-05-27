66b2a2880b0df63eb4294439b40b5f77ff212fba
arexon <me@arexon.dev>
2026-05-27 10:32:41 +0300

    refactor(obs): use unstable package

--- modules/gui/obs.nix
+++ modules/gui/obs.nix
@@ -6,6 +6,7 @@
   }: {
     programs.obs-studio = {
       enable = true;
+      package = pkgs.unstable.obs-studio;
       plugins = with pkgs.obs-studio-plugins; [
         obs-pipewire-audio-capture
         input-overlay

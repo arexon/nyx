4dc817b1771ace5cf06c80a0758c39a6378989ff
arexon <me@arexon.dev>
2026-09-15 17:01:38 +0300

    refactor(ai): remove local models

--- modules/hosts/hydra/configuration.nix
+++ modules/hosts/hydra/configuration.nix
@@ -1,6 +1,5 @@
 {config, ...}: {
   flake.modules.darwin."hosts/hydra".imports = with config.flake.modules.darwin; [
-    ai
     core
     shell
     arexon
--- modules/hosts/hydra/home.nix
+++ modules/hosts/hydra/home.nix
@@ -1,6 +1,5 @@
 {config, ...}: {
   flake.modules.homeManager."homes/arexon@hydra".imports = with config.flake.modules.homeManager; [
-    ai
     arexon
     cli
     core

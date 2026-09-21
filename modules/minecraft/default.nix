35b722215ba6674480b85e21ecc2ee8c4e78bcd9
arexon <me@arexon.dev>
2026-09-22 00:21:57 +0300

    refactor(minecraft): move mods to json file

---
+++ modules/minecraft/default.nix
@@ -0,0 +1,40 @@
+{inputs, ...}: {
+  flake-file.inputs.nix-minecraft = {
+    url = "github:Infinidoge/nix-minecraft";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  flake.modules.nixos.minecraft = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
+
+    nixpkgs.overlays = [inputs.nix-minecraft.overlay];
+
+    # tmux console: tmux -S /run/minecraft/main.sock attach
+    users.users.arexon.extraGroups = ["minecraft"];
+
+    services.minecraft-servers = {
+      enable = true;
+      eula = true;
+      servers.main = {
+        enable = true;
+        package = pkgs.neoforgeServers.neoforge-1_21_1-21_1_249;
+        jvmOpts = "-Xms8G -Xmx8G";
+        serverProperties = {
+          motd = "The Silly World :3";
+          difficulty = "normal";
+          allow-flight = true;
+          max-players = 4;
+          view-distance = 24;
+          simulation-distance = 12;
+        };
+        symlinks.mods = pkgs.linkFarmFromDrvs "mods" (
+          lib.mapAttrsToList (_: pkgs.fetchurl) (lib.importJSON ./mods.json)
+        );
+      };
+    };
+  };
+}
